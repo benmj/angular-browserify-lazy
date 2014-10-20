532a614c331d4b1b2d0755519817e14dc1219def
Ben Jacobs <benmillerj@gmail.com>
2014-10-20 15:22:11 -0700

    updated readme

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -1,10 +1,18 @@
 module.exports = {
-        // write all built files to the dist directory
-        dev: {
-            files: {
-                '<%= yeoman.dist %>/scripts/main.js': ['<%= yeoman.app %>/scripts/app.js', '.tmp/main-templates.js'],
-                '<%= yeoman.dist %>/scripts/oranges.js': ['<%= yeoman.app %>/scripts/oranges/index.js', '.tmp/oranges-templates.js'],
-                '<%= yeoman.dist %>/scripts/apples.js': ['<%= yeoman.app %>/scripts/apples/index.js', '.tmp/apples-templates.js'],
+    // write all built files to the dist directory
+    lib: {
+        files : {
+            '<%= yeoman.dist %>/scripts/vendor.js': [
+                
+            ]
+        }
+    },
+
+    dev: {
+        files: {
+            '<%= yeoman.dist %>/scripts/main.js': ['<%= yeoman.app %>/scripts/app.js', '.tmp/main-templates.js'],
+            '<%= yeoman.dist %>/scripts/oranges.js': ['<%= yeoman.app %>/scripts/oranges/index.js', '.tmp/oranges-templates.js'],
+            '<%= yeoman.dist %>/scripts/apples.js': ['<%= yeoman.app %>/scripts/apples/index.js', '.tmp/apples-templates.js'],
         },
         options: {
             external: ['angular'],
